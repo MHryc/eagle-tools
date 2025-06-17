aeb8424e799cc3a6433827d0a6ed4b98cca92503
mmh <mhry@igr.poznan.pl>
2025-06-17 14:37:57 +0200

    initial commit

---
+++ eagle-tools.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# A set of small tools that I find useful when working on HPC clusters, made
+# accessible by subcommande like 'pinfo'
+
+set -e
+
+#
+# === Help/usage ===
+#
+
+help() {
+cat << EOF
+usage: eagle-tools.sh [-h] {pinfo,cbf} ...
+
+Subcommands:
+pinfo	Get information about available partitions (name, nodes, cpus,
+	memory/node). Takes no arguments.
+
+cbf	"Compare Big File", computes a md5sum of N initial lines (default is
+	1000, can be specified by -s N). Run with -h for help. Gzipped files
+	MUST be passed with -g option
+EOF
+}
+
+#
+# === Subcommand functions ===
+#
+
+pinfo() {
+	#
+	# Partition info
+	# Print info about accessible partitions as a nice table
+	#
+	sinfo -o "%P %D %c %m" | \
+		awk '{printf("%-16s\t%5s\t%10s\t%12s\n", $1, $2, $3, $4)}'
+}
+
+cbf() {
+	#
+	# Compare Big File
+	# prints md5sum of head -n N lines from files. Used to get an idea if
+	# two or more files are likely identical
+	#
+	cbf_help() {
+cat << EOF
+usage: eagle-tools.sh cbf [-s SIZE] [-g] ...
+
+-s	Size (number of lines) that are fed into md5sum. Default is
+	1000.
+
+-g	Use for gzipped files to feed the text content into md5sum.
+EOF
+	}
+
+	size=1000; g_zip=0
+
+	while getopts 's:gh' opt; do
+		case $opt in
+			s) size=${OPTARG} ;;
+			g) g_zip=1 ;;
+			h) cbf_help >&2; exit 0 ;;
+			*) echo 'Something is not right...' >&2; exit 1 ;;
+		esac
+	done
+
+	shift $((OPTIND - 1)); name=$(basename $1)
+
+	[[ g_zip -eq 1 ]] && \
+		md5sum <(zcat $1 | head -n $size) | \
+		awk -v name=$name '{printf("%s\t%s\n", $1, name)}' \
+		|| \
+		md5sum <(head -n $size $1) | \
+		awk -v name=$name '{printf("%s\t%s\n", $1, name)}'
+}
+
+#
+# === Evaluate subcommands ===
+# 
+
+subcmd=$1
+
+case $subcmd in
+	pinfo) shift; pinfo ;;
+
+	cbf) shift; cbf $@ ;;
+
+	*) help >&2; exit 1 ;;
+esac
